e7ed7aa3cb65266f0da7eff1cefe5a59f1abc1e6
anilv8 <81171588+anilv8@users.noreply.github.com>
2022-05-10 20:29:01 +0300

    Update fedora-kde-minimal.sh

--- fedora-kde-minimal.sh
+++ fedora-kde-minimal.sh
@@ -1,76 +1,75 @@
-sudo dnf install \
-  plasma-desktop \ 
-  plasma-workspace
-\ qt5-qtbase-gui
-\ sddm
-\ sddm-breeze
-\ sddm-kcm
-\ NetworkManager-config-connectivity-fedora
-\ PackageKit-command-not-found
-\abrt-desktop
-\ adwaita-gtk2-theme
-\ bluedevil
-\ breeze-icon-theme
-\ colord-kde
-\ cups-pk-helper
-\ dnfdragora
-\ dolphin
-\ firewall-config 
-\ glibc-all-langpacks
-\ gnome-keyring-pam
-\ gwenview
-\ initial-setup-gui
-\ kamera
-\ kcalc
-\ kcharselect
-\ kde-gtk-config
-\ kde-partitionmanager
-\ kde-print-manager
-\ kde-settings-pulseaudio
-\ kdegraphics-thumbnailers
-\ kdeplasma-addons
-\ kdialog
-\ kdnssd
-\ keditbookmarks
-\ kf5-akonadi-server
-\ kf5-akonadi-server-mysql
-\ kf5-baloo-file
-sudo dnf install kf5-kipi-plugins
-sudo dnf install khelpcenter
-sudo dnf install khotkeys
-sudo dnf install kinfocenter
-sudo dnf install kmenuedit
-sudo dnf install konsole5
-sudo dnf install kscreen
-sudo dnf install kscreenlocker
-sudo dnf install ksshaskpass
-sudo dnf install kwebkitpart
-sudo dnf install kwin
-sudo dnf install kwrite
-sudo dnf install okular
-sudo dnf install phonon-qt5-backend-gstreamer
-sudo dnf install pinentry-qt
-sudo dnf install plasma-breeze
-sudo dnf install plasma-desktop-doc
-sudo dnf install plasma-discover
-sudo dnf install plasma-discover-notifier
-sudo dnf install plasma-disks
-sudo dnf install plasma-drkonqi
-sudo dnf install plasma-nm
-sudo dnf install plasma-nm-l2tp
-sudo dnf install plasma-nm-openconnect
-sudo dnf install plasma-nm-openswan
-sudo dnf install plasma-nm-openvpn
-sudo dnf install plasma-nm-pptp
-sudo dnf install plasma-nm-vpnc
-sudo dnf install plasma-pa
-sudo dnf install plasma-systemmonitor
-sudo dnf install plasma-thunderbolt
-sudo dnf install plasma-vault
-sudo dnf install plasma-workspace-geolocation
-sudo dnf install plasma-workspace-xorg
-sudo dnf install polkit-kde
-sudo dnf install qt5-qtdeclarative
-sudo dnf install spectacle
-sudo dnf install xdg-desktop-portal-gnome
-sudo dnf install xorg-x11-drv-libinput
+sudo dnf install  plasma-desktop \
+    plasma-workspace \
+    qt5-qtbase-gui \
+    sddm \
+    sddm-breeze \
+    sddm-kcm \
+    NetworkManager-config-connectivity-fedora \
+    PackageKit-command-not-found \
+    abrt-desktop \
+    adwaita-gtk2-theme \
+    bluedevil \
+    breeze-icon-theme \
+    colord-kde \
+    cups-pk-helper \
+    dnfdragora \
+    dolphin \
+    firewall-config \
+    glibc-all-langpacks \
+    gnome-keyring-pam \
+    gwenview \
+    initial-setup-gui \
+    kamera \
+    kcalc \
+    kcharselect \
+    kde-gtk-config \
+    kde-partitionmanager \
+    kde-print-manager \
+    kde-settings-pulseaudio \
+    kdegraphics-thumbnailers \
+    kdeplasma-addons \
+    kdialog \
+    kdnssd \
+    keditbookmarks \
+    kf5-akonadi-server \
+    kf5-akonadi-server-mysql \
+    kf5-baloo-file \
+    kf5-kipi-plugins \
+    khelpcenter \
+    khotkeys \
+    kinfocenter \
+    kmenuedit \
+    konsole5 \
+    kscreen \
+    kscreenlocker \
+    ksshaskpass \
+    kwebkitpart \
+    kwin \
+    kwrite \
+    okular \
+    phonon-qt5-backend-gstreamer \
+    pinentry-qt \
+    plasma-breeze \
+    plasma-desktop-doc \
+    plasma-discover \
+    plasma-discover-notifier \
+    plasma-disks \
+    plasma-drkonqi \
+    plasma-nm \
+    plasma-nm-l2tp \
+    plasma-nm-openconnect \
+    plasma-nm-openswan \
+    plasma-nm-openvpn \
+    plasma-nm-pptp \
+    plasma-nm-vpnc \
+    plasma-pa \
+    plasma-systemmonitor \
+    plasma-thunderbolt \
+    plasma-vault \
+    plasma-workspace-geolocation \
+    plasma-workspace-xorg \
+    polkit-kde \
+    qt5-qtdeclarative \
+    spectacle \
+    xdg-desktop-portal-gnome \
+    xorg-x11-drv-libinput -y
